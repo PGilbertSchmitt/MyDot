81f168843ae601c4edc1f1f89d2afcff45484c50
P. Gilbert Schmitt <pgilbertschmitt@gmail.com>
2018-09-01 12:33:51 -0700

    Initial commit

---
+++ frontend/react/mydot.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+document.addEventListener("DOMContentLoaded", () => {
+  ReactDOM.render(
+    <h1>This is TS React speaking</h1>,
+    document.getElementById("root"),
+  );
+});
