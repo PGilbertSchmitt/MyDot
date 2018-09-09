879300b46e7f4135aa55c6598f0c39c83f827637
P. Gilbert Schmitt <pgilbertschmitt@gmail.com>
2018-09-09 13:07:32 -0700

    Link removal of window data to closing of tab

--- frontend/react/components/nav/tab_container.ts
+++ frontend/react/components/nav/tab_container.ts
@@ -4,6 +4,7 @@ import { Dispatch } from "redux";
 import { IRootState } from "reducers/_root_reducer";
 import Tab, { IOwnProps, IStateProps, IDispatchProps } from "@nav/tab";
 import { receiveCurrentTab, removeTab } from "actions/tab_actions";
+import { removeWindow } from "actions/content_actions";
 import { ICurrentTab } from "resources/tab";
 
 const mapStateToProps = (state: IRootState, ownProps: IOwnProps): IStateProps => ({
@@ -12,7 +13,11 @@ const mapStateToProps = (state: IRootState, ownProps: IOwnProps): IStateProps =>
 
 const mapDispatchToProps = (dispatch: Dispatch<any>): IDispatchProps => ({
   setCurrentTab: (tab: ICurrentTab) => { dispatch(receiveCurrentTab(tab)); },
-  deleteTab: (id: number) => { dispatch(removeTab(id)); },
+  deleteTab: (id: number) => {
+    dispatch(removeTab(id));
+    // Since tab and content ids are linked by ID, the same id can be used to delete its window.
+    dispatch(removeWindow(id));
+  },
 });
 
 export default connect(
